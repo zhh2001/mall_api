ba61a8108eb39b5e11753fb6d6cf3389a786338f
Zhang Henghua <1652709417@qq.com>
2025-05-10 20:46:18 +0800

    解决跨域问题

--- user_web/initialize/router.go
+++ user_web/initialize/router.go
@@ -2,12 +2,18 @@ package initialize
 
 import (
 	"github.com/gin-gonic/gin"
+
+	"mall_api/user_web/middlewares"
 	router2 "mall_api/user_web/router"
 )
 
 func Routers() *gin.Engine {
 	Router := gin.Default()
+	//配置跨域
+	Router.Use(middlewares.Cors())
+
 	ApiGroup := Router.Group("/u/v1")
 	router2.InitUserRouter(ApiGroup)
+
 	return Router
 }
